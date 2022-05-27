85fc114f065311ec2b59a586a0ddf373edae3deb
GaetanHHHHHH <gaetanherinne@gmail.com>
2022-05-27 23:04:55 +0200

    Last changes for the day

--- angular_project/src/app/change-actualite/change-actualite.component.ts
+++ angular_project/src/app/change-actualite/change-actualite.component.ts
@@ -16,16 +16,18 @@ export class ChangeActualiteComponent implements OnInit {
   actualiteForm!: FormGroup;
   actualitePreview$!: Observable<Actualite>;
   urlRegex!: RegExp;
+  actualiteId = +this.route.snapshot.params['id'];
+  actualite$ = this.ActualitesService.getActualiteById(this.actualiteId);
 
   constructor(private formBuilder: FormBuilder,
     private ActualitesService: ActualitesService,
     private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const actualiteId = +this.route.snapshot.params['id'];
-    const actualite = this.ActualitesService.getActualiteById(actualiteId);
+    // this.ActualitesService.getActualiteById(actualiteId).pipe(map(actualite => ({ ...actualite })));
     //console.log(actualiteId)
     this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+    //console.log(this.actualite$)
     this.actualiteForm = this.formBuilder.group({
       id_users: [null],
       titre: [null],
@@ -36,8 +38,7 @@ export class ChangeActualiteComponent implements OnInit {
     }, {
       updateOn: 'blur'
     });
-    this.actualiteForm.setValue({actualite});
-    this.actualitePreview$ = this. actualiteForm.valueChanges.pipe(
+    this.actualitePreview$ = this.actualiteForm.valueChanges.pipe(
       map(formValue => ({
         ...formValue
       }))
