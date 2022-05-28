2e4fa6a5d548480e1878dd47bc3364e4db9f3ea1
GaetanHHHHHH <gaetanherinne@gmail.com>
2022-05-28 15:34:11 +0200

    Add autofill PUT with existing data

--- angular_project/src/app/change-actualite/change-actualite.component.ts
+++ angular_project/src/app/change-actualite/change-actualite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, map, tap } from 'rxjs';
@@ -12,22 +12,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./change-actualite.component.scss']
 })
 export class ChangeActualiteComponent implements OnInit {
-
+  @Input() actualite!: Actualite;
   actualiteForm!: FormGroup;
   actualitePreview$!: Observable<Actualite>;
+  actualite$!: Observable<Actualite>;
   urlRegex!: RegExp;
-  actualiteId = +this.route.snapshot.params['id'];
-  actualite$ = this.ActualitesService.getActualiteById(this.actualiteId);
+  formFieldData: any;
+  
+  //actualite$ = this.ActualitesService.getActualiteById(this.actualiteId);
 
   constructor(private formBuilder: FormBuilder,
     private ActualitesService: ActualitesService,
     private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // this.ActualitesService.getActualiteById(actualiteId).pipe(map(actualite => ({ ...actualite })));
-    //console.log(actualiteId)
+    const actualiteId = +this.route.snapshot.params['id'];
+    console.log(actualiteId);
+    this.actualite$ = this.ActualitesService.getActualiteById(actualiteId);
+
+
     this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
-    //console.log(this.actualite$)
+    // this.ActualitesService.getActualiteById(actualiteId).subscribe((res:any)=>{
+    // this.formFieldData=res["data"] //to capture the data values to be pre-filled in my form from the backend
+    // console.log(this.formFieldData[0])
+    // })
+    //this.actualiteForm.get("id_users")?.setValue(this.actualite.id_users);
+    //this.actualiteForm.get("titre")?.setValue(this.actualite.titre);
     this.actualiteForm = this.formBuilder.group({
       id_users: [null],
       titre: [null],
